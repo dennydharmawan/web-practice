c670f86f087588a7fcea2cca66c859f02e8b033b
denny dharmawan <denny.dharmawan@krom.id>
2024-01-23 15:00:04 +0700

    completed challenge 444 with custom design

--- src/pages/icodethis/444-simple-pricing.tsx
+++ src/pages/icodethis/444-simple-pricing.tsx
@@ -3,10 +3,117 @@ import { IconCircleCheck } from '@tabler/icons-react';
 
 import { FullScreenLayout } from '@/components/layout/full-screen-layout';
 
+// https://dribbble.com/shots/20089421-Summer-Travel-Price-Plan
 export default function SimplePricingPage() {
   return (
-    <FullScreenLayout>
+    <FullScreenLayout className="bg-[#FF4B01]">
       <div className="super-centered">
+        <section className="content-grid clip aspect-[4/3] h-[92vh] bg-[linear-gradient(to_top_right,_#FF4B01_50%,_#F0B103_50%)] drop-shadow-2xl">
+          <div className="flex flex-col gap-8 py-10">
+            <div className="mx-auto text-center">
+              <h1 className="text-5xl font-bold capitalize">Summer Travel price Plan</h1>
+              <h2 className="text-3xl font-semibold text-slate-700">Pick Your Summer Plan</h2>
+            </div>
+
+            <div className="flex h-full gap-12">
+              <div className="flex h-full w-1/2 flex-col gap-6 rounded-b-3xl rounded-tl-[304px] rounded-tr-xl border-4 border-slate-950 bg-white px-6 py-6 shadow-[10px_10px_0px_rgba(0,0,0,1)]">
+                <section>
+                  <img
+                    src="https://images.pexels.com/photos/3293150/pexels-photo-3293150.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    alt="beach"
+                    className="mb-3 rounded-b-3xl rounded-tl-[304px] rounded-tr-xl object-cover"
+                  />
+
+                  <h1 className="text-2xl font-bold text-purple-700">Coral Paradise Resort</h1>
+                  <p className="text-slate-700">123 Oceanfront Avenue, Seaside Cove, Paradise Island</p>
+                </section>
+
+                {/* [&>ul~ul]:mt-3 */}
+                <section className="pancake-grid">
+                  <List
+                    spacing="xs"
+                    size="sm"
+                    center
+                    icon={
+                      <ThemeIcon color="purple.7" size={24} radius="xl">
+                        <IconCircleCheck size={18} />
+                      </ThemeIcon>
+                    }
+                    className="columns-2 font-semibold text-slate-950"
+                  >
+                    <List.Item>3 night 4 days</List.Item>
+                    <List.Item>4 people</List.Item>
+                    <List.Item>Transport Included</List.Item>
+                    <List.Item>4 Travel spots</List.Item>
+                    <List.Item>Dinner Included</List.Item>
+                    <List.Item>Lunch Included</List.Item>
+                    <List.Item>4 Individual Room</List.Item>
+                    <List.Item>Free Wifi</List.Item>
+                  </List>
+                </section>
+
+                <div className="flex justify-center">
+                  <Button
+                    color="purple.7"
+                    size="xl"
+                    className="border-2 border-slate-950 shadow-[8px_8px_0px_rgba(0,0,0,1)]"
+                  >
+                    Buy Now
+                  </Button>
+                </div>
+              </div>
+
+              <div className="flex h-full w-1/2 flex-col gap-6 rounded-b-3xl rounded-tl-xl rounded-tr-[304px] border-4 border-slate-950 bg-white px-6 py-6 shadow-[10px_10px_0px_rgba(0,0,0,1)]">
+                <section>
+                  <img
+                    src="https://images.pexels.com/photos/3319815/pexels-photo-3319815.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    alt="beach"
+                    className="mb-3 rounded-b-3xl rounded-tl-xl rounded-tr-[304px] object-cover"
+                  />
+
+                  <h1 className="text-2xl font-bold text-pink-700">Sunset Sands Retreat</h1>
+                  <p className="text-slate-700"> 456 Palm Beach Road, Golden Shores, Sun Haven Bay</p>
+                </section>
+
+                {/* [&>ul~ul]:mt-3 */}
+                <section className="pancake-grid">
+                  <List
+                    spacing="xs"
+                    size="sm"
+                    center
+                    icon={
+                      <ThemeIcon color="pink.7" size={24} radius="xl">
+                        <IconCircleCheck size={18} />
+                      </ThemeIcon>
+                    }
+                    className="columns-2 font-semibold text-slate-950"
+                  >
+                    <List.Item>3 night 4 days</List.Item>
+                    <List.Item>4 people</List.Item>
+                    <List.Item>Transport Included</List.Item>
+                    <List.Item>4 Travel spots</List.Item>
+                    <List.Item>Dinner Included</List.Item>
+                    <List.Item>Lunch Included</List.Item>
+                    <List.Item>4 Individual Room</List.Item>
+                    <List.Item>Free Wifi</List.Item>
+                  </List>
+                </section>
+
+                <div className="flex justify-center">
+                  <Button
+                    color="pink.7"
+                    size="xl"
+                    className="border-2 border-slate-950 shadow-[8px_8px_0px_rgba(0,0,0,1)]"
+                  >
+                    Buy Now
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/*
         <div className="flex gap-5">
           <div className="min-w-[20rem] rounded-md border-2 border-black bg-white shadow-[8px_8px_0px_rgba(0,0,0,1)]">
             <div className="block cursor-pointer">
@@ -73,7 +180,7 @@ export default function SimplePricingPage() {
               </section>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </FullScreenLayout>
   );

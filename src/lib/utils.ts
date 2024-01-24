8ae5d51f45c4d83293689db579ba511441438aa4
denny dharmawan <denny.dharmawan@krom.id>
2024-01-24 16:33:12 +0700

    created scripts for icodethis route generation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,5 @@
 import { clsx, type ClassValue } from 'clsx';
-import { RouteObject } from 'react-router-dom';
 import { twMerge } from 'tailwind-merge';
-
-import { Path } from '@/router';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -10,60 +7,4 @@ export function cn(...inputs: ClassValue[]) {
 
 export function padNumber(number: number) {
   return String(number).padStart(2, '0');
-}
-
-type PageObject = {
-  path: Path;
-  challengeNumber: string;
-  title: string;
-  id: string;
-};
-
-export function getRoutes(routes: RouteObject): PageObject[] {
-  const paths: Path[] = [];
-
-  const traverse = (element: RouteObject) => {
-    if (element.children && element.children.length > 0) {
-      element.children.forEach((child) => traverse(child));
-    } else if (element.id && typeof element.id === 'string') {
-      paths.unshift(('/' + element.id) as Path);
-    }
-  };
-
-  traverse(routes);
-
-  const sortedPaths = paths.sort((a, b) => a.localeCompare(b, undefined, { numeric: true }));
-
-  const pageObjects: PageObject[] = sortedPaths
-    .map((path) => ({
-      path,
-      challengeNumber: extractChallengeNumberFromPath(path),
-      title: extractTitle(path),
-      id: extractId(path)
-    }))
-    .filter((obj) => obj.challengeNumber !== '');
-
-  return pageObjects;
-}
-
-export function extractChallengeNumberFromPath(path: string): string {
-  const match = path.match(/\/icodethis\/(\d+)-[a-z-]+/);
-
-  return match ? match[1] : '';
-}
-
-export function extractTitle(path: string): string {
-  const match = path.match(/\/icodethis\/\d+-(.+)$/);
-
-  return match ? titleCase(match[1].replace(/-/g, ' ')) : ''; // Replace hyphens with spaces
-}
-
-export function extractId(path: string): string {
-  const match = path.match(/\/icodethis\/\d+-(.+)$/);
-
-  return match ? match[1].replace(/-/g, '-') : ''; // Replace hyphens with dash
-}
-
-export function titleCase(str: string): string {
-  return str.replace(/\w\S*/g, (word) => word.charAt(0).toUpperCase() + word.substr(1).toLowerCase());
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,58 +1,63 @@
 import { Link } from 'react-router-dom';
 
-import { routes } from '@generouted/react-router';
 import { Button, Card } from '@mantine/core';
 import { IconExternalLink, IconEye } from '@tabler/icons-react';
 
 import { FullScreenLayout } from '@/components/layout/full-screen-layout';
-import { getRoutes } from '@/lib/utils';
+import pageObjects from '@/data/icodethis.json';
 
 export default function index() {
-  const pageObjects = getRoutes(routes[0]);
-  console.log(pageObjects);
-
   return (
     <FullScreenLayout>
       <main className="content-grid py-8">
         <div>
           <div className="pancake-grid items-start [--custom-gap:1.5rem] [--custom-min:320px]">
-            {pageObjects
-              // .filter((pageObject) => pageObject !== '/' && route.startsWith('/icodethis'))
-              .map((pageObject) => (
-                <section aria-labelledby={pageObject.id} key={pageObject.id}>
-                  <Card component="a" href={pageObject.path} padding="sm" shadow="sm" target="_blank">
-                    {/* <Card.Section>
+            {pageObjects.map((pageObject) => (
+              <section aria-labelledby={pageObject.id} key={pageObject.id}>
+                <Card
+                  className="gap-4"
+                  component="a"
+                  href={pageObject.path}
+                  padding="sm"
+                  shadow="sm"
+                  target="_blank"
+                >
+                  {/* <Card.Section>
                   <Image
                     src="https://images.unsplash.com/photo-1579227114347-15d08fc37cae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
                     h={160}
                     alt="No way!"
                   />
                 </Card.Section> */}
-                    <h3 className="text-2xl font-bold text-primary" id={pageObject.id}>
+                  <div>
+                    <h3 className="line-clamp-1 text-2xl font-bold text-primary" id={pageObject.id}>
                       {pageObject.title}
                     </h3>
-                    <div className="pancake-flexbox mt-auto [--custom-gap:1rem] [--custom-min:160px]">
-                      <Button
-                        component={Link}
-                        leftSection={<IconEye size={20} />}
-                        target="_blank"
-                        to={pageObject.path}
-                      >
-                        View Solution
-                      </Button>
-                      <Button
-                        component={Link}
-                        leftSection={<IconExternalLink size={20} />}
-                        target="_blank"
-                        to={`https://icodethis.com/challenges/${pageObject.challengeNumber}`}
-                        variant="light"
-                      >
-                        Visit Challenge
-                      </Button>
-                    </div>
-                  </Card>
-                </section>
-              ))}
+                    <p className="text-slate-600">Challenge Number {`#${pageObject.challengeNumber}`}</p>
+                  </div>
+
+                  <div className="pancake-flexbox mt-auto [--custom-gap:1rem] [--custom-min:160px]">
+                    <Button
+                      component={Link}
+                      leftSection={<IconEye size={20} />}
+                      target="_blank"
+                      to={pageObject.path}
+                    >
+                      View Solution
+                    </Button>
+                    <Button
+                      component={Link}
+                      leftSection={<IconExternalLink size={20} />}
+                      target="_blank"
+                      to={`https://icodethis.com/challenges/${pageObject.challengeNumber}`}
+                      variant="light"
+                    >
+                      Visit Challenge
+                    </Button>
+                  </div>
+                </Card>
+              </section>
+            ))}
           </div>
         </div>
       </main>

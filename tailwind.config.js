338a73cf32e9c7dd0d4d18a8aab924ac44b9ecd1
Denny Dharmawan <dennydharmawan@github.com>
2024-01-27 23:09:21 +0700

    added beam and grid pattern to header

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,11 @@ export default {
     extend: {
       screens: {
         xs: '320px'
+      },
+      backgroundImage: {
+        'grid-white': `url(
+          "data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='rgb(255 255 255 / 0.06)'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e"
+        )`
       }
     }
   },

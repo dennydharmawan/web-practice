b2f488eec3024b5766c4049cd00d22ae77e8fe95
denny dharmawan <denny.dharmawan@krom.id>
2024-02-11 23:48:26 +0700

    moved card to framer-motion

--- src/router.ts
+++ src/router.ts
@@ -5,7 +5,7 @@ import { components, hooks, utils } from '@generouted/react-router/client';
 
 export type Path =
   | `/`
-  | `/animation/card`
+  | `/animation/framer-motion/card`
   | `/animation/framer-motion/header-logo`
   | `/animation/framer-motion/switch`
   | `/animation/hamburger-menu`
